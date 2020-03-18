4ba0a95321dc34afedf4f40a62e90df4c48b1fd2
Yuri Kolot <kolot.ius@students.dvfu.ru>
2020-03-18 16:23:55 +1000

    Update Swagger config

--- src/main/java/com/arrayteam/argo/server/configuration/SwaggerConfiguration.java
+++ src/main/java/com/arrayteam/argo/server/configuration/SwaggerConfiguration.java
@@ -3,12 +3,21 @@ package com.arrayteam.argo.server.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 
 
 @Configuration
@@ -28,11 +37,51 @@ public class SwaggerConfiguration {
                                 .build()
                 )
                 .pathMapping("/")
+                .produces(new HashSet<>(Collections.singletonList("application/json")))
+                .consumes(new HashSet<>(Collections.singletonList("application/json")))
+                .globalResponseMessage(RequestMethod.GET, responseMessages())
+                .globalResponseMessage(RequestMethod.POST, responseMessages())
+                .globalResponseMessage(RequestMethod.PUT, responseMessages())
+                .globalResponseMessage(RequestMethod.PATCH, responseMessages())
+                .globalResponseMessage(RequestMethod.DELETE, responseMessages())
                 .tags(
                         new Tag("TargetController", "Control work with markers"),
                         new Tag("ARCController", "Control the type of tracking, markers and virtual content"),
                         new Tag("VirtualContentController", "Control work with virtual content")
                 );
+    }
+
+    private List<ResponseMessage> responseMessages() {
+        return Arrays.asList(
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.OK.value())
+                        .message("OK")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.NO_CONTENT.value())
+                        .message("No content")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.BAD_REQUEST.value())
+                        .message("Bad request")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.UNAUTHORIZED.value())
+                        .message("You are not authorized to view the resource")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.FORBIDDEN.value())
+                        .message("Accessing the resource you were trying to reach is forbidden")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.NOT_FOUND.value())
+                        .message("The resource you were trying to reach is not found")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                        .message("Internal error")
+                        .build()
+        );
     }
 
 }
