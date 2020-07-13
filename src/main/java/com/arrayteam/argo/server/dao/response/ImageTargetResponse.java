565160ee78228a2fb549199ab1ae9de690d7177c
Yuri Kolot <kolot.ius@students.dvfu.ru>
2020-07-13 15:15:35 +1000

    Disable tests

--- src/main/java/com/arrayteam/argo/server/dao/response/ImageTargetResponse.java
+++ src/main/java/com/arrayteam/argo/server/dao/response/ImageTargetResponse.java
@@ -4,7 +4,7 @@ package com.arrayteam.argo.server.dao.response;
 import com.arrayteam.argo.server.dao.model.ImageTarget;
 
 
-public class ImageTargetResponse extends Response<ImageTarget, ImageTargetResponse> {
+public final class ImageTargetResponse extends Response<ImageTarget, ImageTargetResponse> {
 
     public ImageTargetResponse() {
         super(ImageTargetResponse.class);
